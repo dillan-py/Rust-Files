b8341700b93865eada2d909f081059f60ca7f69b
dillan-py <149972792+dillan-py@users.noreply.github.com>
2025-04-01 00:52:30 +0100

    Create intro.rs

---
+++ intro.rs
@@ -0,0 +1,19 @@
+// This is a comment, and is ignored by the compiler.
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
+// shortcut.
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+   // println! is a macro that prints text to the console.
+    println!("This is my first rust program i have executed\nCompile the file with: rustc <file.rs> then run with ./<file.exe>")
+  // Macros in Rust are a feature that allows developers to write code that generates other code, a technique known as metaprogramming...
+  //... They are evaluated at compile time, not runtime, which can lead to more efficient code
+}
